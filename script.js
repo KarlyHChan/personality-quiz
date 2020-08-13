d17a4d9549178a103f655335cfaf7c06bfc54948
Glitch (personality-quiz-starter-code) <none>
2020-08-13 14:22:37 +0000

    🏖🐙 Checkpoint ./script.js:20325048/171

--- script.js
+++ script.js
@@ -14,14 +14,17 @@ var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
+
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
 var q2a3 = document.getElementById("q2a3");
 var q2a4 = document.getElementById("q2a4");
+
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 var q3a4 = document.getElementById("q3a4");
+
 var q4a1 = document.getElementById("q4a1");
 var q4a2 = document.getElementById("q4a2");
 var q4a3 = document.getElementById("q4a3");
@@ -31,7 +34,10 @@ var q4a4 = document.getElementById("q4a4");
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
-q1a1.addEventListener("click", result1);
+q1a1.addEventListener("click", hermione);
+q1a2.addEventListener("click", elle);
+q1a3.addEventListener("click", katniss);
+q1a4.addEventListener("click", leia);
 
 
 
